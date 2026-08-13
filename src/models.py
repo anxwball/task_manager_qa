71fb5bda08bd4076e09c0edb62c1f697d1b22f83
anxwball <aaron.nxwball@gmail.com>
2026-08-12 21:17:44 -0500

    feat(models): crear el modelo Task del dominio

---
+++ src/models.py
@@ -0,0 +1,14 @@
+# src/models.py
+
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import declarative_base
+
+Base = declarative_base()
+
+class Task(Base):
+    __tablename__ = 'tasks'
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String, index=True)
+    description = Column(String, index=True)
+    status = Column(String, index=True)
